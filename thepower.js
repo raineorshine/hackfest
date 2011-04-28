c4fe6181e3167f06c25b4f203cda0037c7346383
nicoquickleft <nico@quickleft.com>
2011-04-27 18:00:25 -0600

    April 2011 - JavaScript - by Nico Valencia GET YOUR ZOMBIE WHALE ON

---
+++ thepower.js
@@ -0,0 +1,36 @@
+/**
+ *  _   _    _    ____ _  _______ _____ ____ _____ 
+ * | | | |  / \  / ___| |/ /  ___| ____/ ___|_   _|
+ * | |_| | / _ \| |   | ' /| |_  |  _| \___ \ | |  
+ * |  _  |/ ___ \ |___| . \|  _| | |___ ___) || |  
+ * |_| |_/_/   \_\____|_|\_\_|   |_____|____/ |_|  
+ *
+ * QuickLeft Hackfest
+ * April 27 2011
+ *
+ * JavaScript
+ * Written by, Nico Valencia
+ *
+ * @module HACKFEST
+ */
+
+(function( window, undefined ){
+
+  var HACKFEST = {
+
+    init: function () {
+
+      // Begin your mad JavaScript in here!
+
+    },
+
+    anotherFunction: function () {
+
+
+    }
+
+  };
+
+  $( document ).ready( HACKFEST.init );
+
+})( this );
